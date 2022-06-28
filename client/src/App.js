d4e07798a0debfe78103e45593cdc4854303e5ef
basildybala <akbasil123@gmail.com>
2022-06-28 13:04:21 +0530

    Client side Working

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import { io } from "socket.io-client";
 const socket = io(`http://localhost:3001`);
+import './app.css'
+import chatlogo from "./images/chat.png";
+
 
 const App = () => {
   return (
-    <div>Chat App</div>
+    <div className='App'>
+      <header className="app-header">
+        <img src={chatlogo} alt="" />
+        <div className="app-name b-500 primaryColor">My Chat</div>
+      </header>
+    </div>
   )
 }
 
